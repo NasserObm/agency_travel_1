45f3b62e3e034b22e75574ab1267ba6b19098d86
NasserObm <nassermohamedobame@gmail.com>
2024-10-21 18:07:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# agency_travel_1
+ Une appliczation mobile de voyage réaliser en Flutter
---
+++ android/app/src/main/kotlin/com/example/agency_travel_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.agency_travel_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
